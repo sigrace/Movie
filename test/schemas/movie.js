5757a1e6bd5d33ea7243daa074951b7c331f8b56
amy <amy@amydeMacBook-Pro.local>
2015-05-11 13:48:45 +0800

    Imooc movie 第一版

---
+++ test/schemas/movie.js
@@ -0,0 +1,60 @@
+var mongoose = require('mongoose');
+
+var MovieSchema = new mongoose.Schema({
+	doctor:String,
+	title:String,
+	language:String,
+	country:String,
+	summary:String,
+	flash:String,
+	poster:String,
+	year:Number,
+	meta:{
+		createAt:{
+			type: Date,
+			default: Date.now()
+		},
+		updateAt:{
+			type:Date,
+			default:Date.now()
+		}
+	}
+});
+
+MovieSchema.pre('save',function (next) {
+	if(this.isNew){
+		this.meta.createAt = this.meta.updateAt = Date.now();
+	}
+	else{
+		this.meta.updateAt = Date.now();
+	}
+	next();
+});
+
+MovieSchema.statics = {
+	fetch: function (cb){             //取出目前数据可里的所有数据
+		return this
+			.find({})          
+			.sort('meta.updateAt')     //按更新时间排序
+			.exec(cb);                //执行回调方法
+	},
+	findById: function (id,cb){
+		return this
+			.findOne({_id:id})
+			.exec(cb)
+	}
+}
+
+module.exports = MovieSchema;
+
+
+
+
+
+
+
+
+
+
+
+
