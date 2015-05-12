f75b28fec16a7be677315acfd9464491b15541e3
amy <amy@amydeMacBook-Pro.local>
2015-05-12 11:19:13 +0800

    增加登陆注册跳转页面

---
+++ test/app/controllers/index.js
@@ -0,0 +1,15 @@
+var Movie = require('../models/movie');
+
+exports.index = function (req,res){
+	// console.log('user in session');
+	// console.log(req.session.user);
+	Movie.fetch(function (err,movies){
+		if(err){
+			console.log(err);
+		}
+		res.render('index',{
+			title:'imooc 首页',
+			movies: movies
+		});
+	})
+};
