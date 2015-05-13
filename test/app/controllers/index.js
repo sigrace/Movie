fcde05aaefeaf4e73586464979c95bee948d7e99
Amy <593011957@qq.com>
2015-05-13 15:53:24 +0800

    增加搜索功能

--- test/app/controllers/index.js
+++ test/app/controllers/index.js
@@ -19,30 +19,52 @@ exports.index = function (req,res){
 
 exports.search = function (req,res){
 	var catId = req.query.cat;
-	var page = parseInt(req.query.p,10);
+	var q = req.query.q;
+	var page = parseInt(req.query.p,10) || 0;
 	var count = 2;
 	var index = page * count;
 
-	Catetory
-		.find({_id: catId})
-		.populate({
-			path:'movies', 
-			select: 'title poster'
-		})
-		.exec(function (err,catetories){
-			if(err){
-				console.log(err);
-			}
-			var catetory = catetories[0] || {};
-			var movies = catetory.movies || [];
-			var results = movies.slice(index,index + count);
-			res.render('results',{
-				title: 'imooc 结果列表页面',
-				keyword: catetory.name,
-				currentPage :(page + 1),
-				query: 'cat=' + catId,
-				totalPage : Math.ceil(movies.length / count),
-				movies: results
-			});
-		})
+	if(catId){
+		Catetory
+			.find({_id: catId})
+			.populate({
+				path:'movies', 
+				select: 'title poster'
+			})
+			.exec(function (err,catetories){
+				if(err){
+					console.log(err);
+				}
+				var catetory = catetories[0] || {};
+				var movies = catetory.movies || [];
+				var results = movies.slice(index,index + count);
+				
+				res.render('results',{
+					title: 'imooc 结果列表页面',
+					keyword: catetory.name,
+					currentPage :(page + 1),
+					query: 'cat=' + catId,
+					totalPage : Math.ceil(movies.length / count),
+					movies: results
+				});
+			})
+	}
+	else{
+		Movie
+			.find({title: new RegExp(q + '.*','i')})
+			.exec(function (err,movies){
+				if(err){
+					console.log(err);
+				}
+				var results = movies.slice(index, index + count);
+				res.render('results',{
+					title: 'imooc 结果列表页面',
+					keyword:q,
+					currentPage: (page+1),
+					query: 'q='+q,
+					totalPage: Math.ceil(movies.length / count),
+					movies: results
+				})
+			})
+	}
 };
